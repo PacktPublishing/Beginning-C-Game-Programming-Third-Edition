b06bb1ffaa22bfb7b815c006e44770214feceb9c
Simanta Rajbangshi <165140129+Simanta-R@users.noreply.github.com>
2024-05-24 17:46:44 +0530

    Add files via upload

---
+++ Chapter1/Timber.cpp
@@ -0,0 +1,69 @@
+// Include important libraries here
+#include <SFML/Graphics.hpp>
+
+// Make code easier to type with "using namespace" 
+using namespace sf;
+
+// This is where our game starts from int main()
+int main()
+{
+	// Create a video mode object 
+	VideoMode vm(1920, 1080);
+
+	// Create and open a window for the game
+	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+
+	// Create a texture to hold a graphic on the GPU 
+	Texture textureBackground;
+
+	// Load a graphic into the texture 
+	textureBackground.loadFromFile("graphics/background.png");
+
+	// Create a sprite 
+	Sprite spriteBackground;
+
+	// Attach the texture to the sprite 
+	spriteBackground.setTexture(textureBackground);
+
+	// Set the spriteBackground to cover the screen spriteBackground.setPosition(0,0);
+
+
+	while (window.isOpen())
+	{
+
+		/*
+		**************************************** 
+		Handle the players input
+		****************************************
+		*/
+
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		/*
+		**************************************** 
+		Update the scene
+		****************************************
+		*/
+
+
+		/*
+		**************************************** 
+		Draw the scene
+		****************************************
+		*/
+
+		// Clear everything from the last frame 
+		window.clear();
+
+		// Draw our game scene here
+		window.draw(spriteBackground);
+
+		// Show everything we just drew 
+		window.display();
+	}
+	return 0;
+}
+
