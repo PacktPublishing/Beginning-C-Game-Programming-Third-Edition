237d166c9f1eb9ebdc6375d2230991be76db89e8
Simanta Rajbangshi <165140129+Simanta-R@users.noreply.github.com>
2024-05-24 19:21:49 +0530

    Delete Chapter 12 directory

--- Chapter 12/ZombieArena.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Zombie.h"
-#include <SFML/Graphics.hpp>
-using namespace sf;
-
-int createBackground(VertexArray& rVA, IntRect arena);
-Zombie* createHorde(int numZombies, IntRect arena);
